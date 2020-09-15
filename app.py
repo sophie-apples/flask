0c91fc533db44938f7c0b31198e1c4df4171ad54
sophie-apples <sophieannemurray@gmail.com>
2020-09-15 12:17:15 +0000

    added requirements.txt file

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "Hello World"
+@app.route('/')
+def hello():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
